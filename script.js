7b9bcf0359825ed12696da5151e63a552e08bd64
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-28 14:37:26 -0600

    added the prep code for the lecture

---
+++ script.js
@@ -0,0 +1,82 @@
+//  COMMENTS
+
+//  // notes a single line comment
+
+/* 
+Notes a multi-line comment
+*/
+
+// LINKING SCRIPT
+
+/*
+<script src="script.js"> </script>
+*/
+
+// VARIABLES
+var x;
+var y = "Hi Mom";
+
+// DATATYPES            
+// Can use 'typeof' keyword to determine the type of a variable
+
+
+
+var string = "String";
+var number = 1234;
+var boolean = true; //(false)
+var undefined;
+var nullVariable = null;
+var object = {
+	key: "value",
+}; // Can be an array, an object, or a date
+var date = new Date("2023-3-28");
+var array = ["Joe", "Eli", "Henry", "Morgan", "Matthew"];
+var bigInt = BigInt("123456789012345678901234567890"); //used to store integer values that are too big to be represented by Number datatype
+var symbol = Symbol("foo");
+
+
+
+
+
+
+// FUNCTIONS
+
+// name    perameters go in ()   logic goes in {}
+// function exampleFunction() {
+// 	console.log("This is from within the exampleFunction");
+// 	console.log(typeof string);
+// }
+// exampleFunction();
+
+
+
+
+
+// IF STATEMENTS
+// if (typeof array === "object") {
+// 	console.log("variable is an object datatype!");
+// } else {
+// 	console.log("variable is not an object datatype!");
+// }
+
+
+
+
+
+// FOR
+// console.log(array);
+// for (var i = 0; i < array.length; i++) {
+// 	console.log(array[i]);
+// }
+
+
+
+
+
+// METHODS
+
+// METHODS are functions that are build into javascript
+// console.log(array.reverse());
+// console.log(string.split(""));
+// number = number.toString();
+// console.log(typeof number);
